ae001a98796880a3201c5ce1fa110f5444799f46
devrathoree <120649368+devrathoree@users.noreply.github.com>
2023-03-19 23:03:25 +0530

    Update add2.sh

--- add2.sh
+++ add2.sh
@@ -1,14 +1,3 @@
-pipeline {
-    agent any
-    stages {
-        stage('Add') {
-            steps {
-                 echo "Value of a:${params.a}"
-                 echo "Value of b:${params.b}"
-                 sum=${params.a} + ${params.b}
-                 echo "sum:${sum}"
-                
-            }
-        }
-    }
-}
+echo a:$a
+echo "b=:$b"
+sum=$(( $a + $b ))
